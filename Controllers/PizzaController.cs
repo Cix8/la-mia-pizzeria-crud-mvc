406c3f7d2ac6f678723a908587689fa8d21b7fb3
Cix8 <francesco.natal_fn@libero.it>
2022-10-06 14:33:45 +0200

    add Update in Controller & Views

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -9,61 +9,56 @@ namespace la_mia_pizzeria_static.Controllers
     public class PizzaController : Controller
     {
         private readonly ILogger<PizzaController> _logger;
+        private Pizzeria _pizzeria_db;
 
         public PizzaController(ILogger<PizzaController> logger)
         {
             _logger = logger;
+            _pizzeria_db = new Pizzeria();
         }
 
         private void PizzaSeeder()
         {
-            using (Pizzeria pizzeria_db = new Pizzeria())
-            {
-                PizzaModel newPizza = new PizzaModel("Margherita", "La classica pizza napoletana", "pizza-margherita.jfif", 5.99F);
-                pizzeria_db.Add(newPizza);
-                pizzeria_db.SaveChanges();
-                
-                newPizza = new PizzaModel("Capricciosa", "La pizza capricciosa è una pizza tipica della cucina italiana caratterizzata da un condimento di pomodoro, mozzarella, prosciutto cotto, funghi, olive verdi e nere, e carciofini", "pizza-capricciosa.jfif", 7.99F);
-                pizzeria_db.Add(newPizza);
-                pizzeria_db.SaveChanges();
+            PizzaModel newPizza = new PizzaModel("Margherita", "La classica pizza napoletana", "pizza-margherita.jfif", 5.99F);
+            _pizzeria_db.Add(newPizza);
+            _pizzeria_db.SaveChanges();
 
-                newPizza = new PizzaModel("Salame Piccante", "Pizza base margherita con aggiunta di salame piccante", "pizza-salame.jfif", 6.99F);
-                pizzeria_db.Add(newPizza);
-                pizzeria_db.SaveChanges();
-            }
+            newPizza = new PizzaModel("Capricciosa", "La pizza capricciosa è una pizza tipica della cucina italiana caratterizzata da un condimento di pomodoro, mozzarella, prosciutto cotto, funghi, olive verdi e nere, e carciofini", "pizza-capricciosa.jfif", 7.99F);
+            _pizzeria_db.Add(newPizza);
+            _pizzeria_db.SaveChanges();
+
+            newPizza = new PizzaModel("Salame Piccante", "Pizza base margherita con aggiunta di salame piccante", "pizza-salame.jfif", 6.99F);
+            _pizzeria_db.Add(newPizza);
+            _pizzeria_db.SaveChanges();
         }
 
         private void Store(PizzaModel pizza)
         {
-            using (Pizzeria pizzeria_db = new Pizzeria())
-            {
-                pizzeria_db.Add(pizza);
-                pizzeria_db.SaveChanges();
-            }
+            _pizzeria_db.Add(pizza);
+            _pizzeria_db.SaveChanges();
+        }
+
+        public PizzaModel FindBy(int id)
+        {
+            return _pizzeria_db.Pizzas.Find(id);
         }
 
         public IActionResult Index()
         {
             List<PizzaModel> pizzaList = new List<PizzaModel>();
-            using (Pizzeria pizzeria_db = new Pizzeria())
+            pizzaList = _pizzeria_db.Pizzas.OrderBy(pizza => pizza.Id).ToList<PizzaModel>();
+            if (pizzaList.Count == 0)
             {
-                pizzaList = pizzeria_db.Pizzas.OrderBy(pizza => pizza.Id).ToList<PizzaModel>();
-                if(pizzaList.Count == 0)
-                {
-                    this.PizzaSeeder();
-                    pizzaList = pizzeria_db.Pizzas.OrderBy(pizza => pizza.Id).ToList<PizzaModel>();
-                }
+                this.PizzaSeeder();
+                pizzaList = _pizzeria_db.Pizzas.OrderBy(pizza => pizza.Id).ToList<PizzaModel>();
             }
             return View(pizzaList);
         }
 
         public IActionResult Details(int id)
         {
-            using (Pizzeria pizzeria_db = new Pizzeria())
-            {
-                PizzaModel thisPizza = pizzeria_db.Pizzas.Find(id);
-                return View("Show", thisPizza);
-            }
+            PizzaModel thisPizza = this.FindBy(id);
+            return View("Show", thisPizza);
         }
 
         [HttpGet]
@@ -81,6 +76,44 @@ namespace la_mia_pizzeria_static.Controllers
                 return View("Create", model);
             }
             this.Store(new PizzaModel(model.Name, model.Description, model.Image, (float)model.Price));
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            PizzaModel thisPizza = this.FindBy(id);
+             if(thisPizza != null)
+            {
+                return View(thisPizza);
+            } else
+            {
+                return NotFound("Non siamo riusciti a trovare la pizza selezionata...");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, PizzaModel pizza)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Update", pizza);
+            }
+
+            PizzaModel thisPizza = this.FindBy(id);
+            if(thisPizza != null)
+            {
+                thisPizza.Name = pizza.Name;
+                thisPizza.Description = pizza.Description;
+                thisPizza.Image = pizza.Image;
+                thisPizza.Price = pizza.Price;
+
+                _pizzeria_db.SaveChanges();
+            } else
+            {
+                return NotFound("Non siamo riusciti a trovare la pizza da aggiornare");
+            }
             return RedirectToAction("Index");
         }
 
