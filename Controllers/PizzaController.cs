bf3f7c523f0d8fbfcf82409e6db30aceee9ec94e
Cix8 <francesco.natal_fn@libero.it>
2022-10-06 14:07:22 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,98 @@
+﻿using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.MyDbContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        private void PizzaSeeder()
+        {
+            using (Pizzeria pizzeria_db = new Pizzeria())
+            {
+                PizzaModel newPizza = new PizzaModel("Margherita", "La classica pizza napoletana", "pizza-margherita.jfif", 5.99F);
+                pizzeria_db.Add(newPizza);
+                pizzeria_db.SaveChanges();
+                
+                newPizza = new PizzaModel("Capricciosa", "La pizza capricciosa è una pizza tipica della cucina italiana caratterizzata da un condimento di pomodoro, mozzarella, prosciutto cotto, funghi, olive verdi e nere, e carciofini", "pizza-capricciosa.jfif", 7.99F);
+                pizzeria_db.Add(newPizza);
+                pizzeria_db.SaveChanges();
+
+                newPizza = new PizzaModel("Salame Piccante", "Pizza base margherita con aggiunta di salame piccante", "pizza-salame.jfif", 6.99F);
+                pizzeria_db.Add(newPizza);
+                pizzeria_db.SaveChanges();
+            }
+        }
+
+        private void Store(PizzaModel pizza)
+        {
+            using (Pizzeria pizzeria_db = new Pizzeria())
+            {
+                pizzeria_db.Add(pizza);
+                pizzeria_db.SaveChanges();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            List<PizzaModel> pizzaList = new List<PizzaModel>();
+            using (Pizzeria pizzeria_db = new Pizzeria())
+            {
+                pizzaList = pizzeria_db.Pizzas.OrderBy(pizza => pizza.Id).ToList<PizzaModel>();
+                if(pizzaList.Count == 0)
+                {
+                    this.PizzaSeeder();
+                    pizzaList = pizzeria_db.Pizzas.OrderBy(pizza => pizza.Id).ToList<PizzaModel>();
+                }
+            }
+            return View(pizzaList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            using (Pizzeria pizzeria_db = new Pizzeria())
+            {
+                PizzaModel thisPizza = pizzeria_db.Pizzas.Find(id);
+                return View("Show", thisPizza);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(PizzaModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", model);
+            }
+            this.Store(new PizzaModel(model.Name, model.Description, model.Image, (float)model.Price));
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/PizzaModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using la_mia_pizzeria_static.MyValidations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(30, ErrorMessage = "Il nome non può avere più di 30 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Column(TypeName = "text")]
+        [DescriptionValidation]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Column(TypeName = "text")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [PriceValidation]
+        public float Price { get; set; }
+
+        public PizzaModel()
+        {
+
+        }
+
+        public PizzaModel(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
